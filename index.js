2dcd2636fa97c08206aa65e51421b44ac23e7749
BjornGrylls <bjornskole@gmail.com>
2022-02-17 20:47:41 +0100

    Recreated bot. Official guide was insufficient

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
 
 client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+client.on('message', message => {
+    console.log(message.content)
+    if (message.content == "ping") {
 
-    const { commandName } = interaction;
-
-    if (commandName === 'ping') {
-        await interaction.reply('Pong!');
-    } else if (commandName === 'server') {
-        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
-    } else if (commandName === 'user') {
-        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+        message.channel.send("pong")
     }
 });
 
