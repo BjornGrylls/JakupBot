b18da67d255a4837743fac919678216d666f80d0
BjornGrylls <bjornskole@gmail.com>
2022-02-21 11:17:58 +0100

    Added webserver for Heroku

---
+++ app.js
@@ -0,0 +1,14 @@
+// create an express app
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 80085
+
+// define the first route
+app.get("/", function(req, res) {
+    res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
+app.listen(port,
+    () => console.log("Server is running... on " + port)
+);
