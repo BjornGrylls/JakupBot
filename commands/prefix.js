efd1c3fe65f407dbfca63f33226b34f42611c915
BjornGrylls <bjornskole@gmail.com>
2022-02-22 13:13:29 +0100

    Oprydning i kode

--- commands/prefix.js
+++ commands/prefix.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'command to change prefix',
     execute(message, args) {
         var formattedmessage
-        if (args.length != 0) formattedmessage = 'Changed prefix to ' + args[0]
+        if (args.length != 0) formattedmessage = `Changed prefix to "${args[0]}"`
         else formattedmessage = 'You need to note a new prefix like "!prefix ?". Prefix wasn\'t changed'
         const env = process.env.env
         if (env) {
